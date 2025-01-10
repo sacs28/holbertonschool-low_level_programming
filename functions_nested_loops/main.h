2c1831e1c7df75ff1cfe77190fa53323a0d694cd
sacs28 <mi_el_28@hotmail.com>
2025-01-10 07:20:58 +0000

    Write a function that computes the absolute value of an integer

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,5 +9,11 @@ int _isalpha(int c);
 int _putchar(char c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
 #endif /* MAIN_H */
