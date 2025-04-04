2feccbc8a61b2cfa5201173d9658d893bb07d582
sacs28 <mi_el_28@hotmail.com>
2025-04-04 11:47:17 -0500

    Write a script that generates the assembly code

---
+++ hello_world/.s
@@ -0,0 +1,20 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 15, 0	sdk_version 15, 4
+	.intel_syntax noprefix
+	.globl	_main                           ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	mov	dword ptr [rbp - 4], 0
+	xor	eax, eax
+	pop	rbp
+	ret
+	.cfi_endproc
+                                        ## -- End function
+.subsections_via_symbols
