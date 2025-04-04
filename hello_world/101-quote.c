a5bec470705ac3312813f1d7b275bdabe346eb1a
sacs28 <mi_el_28@hotmail.com>
2025-04-04 13:44:13 -0500

    Write a C program that prints exactly a text  followed by a new line, to the standard error

---
+++ hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print a message to standard error
+ *
+ * Description: Write a C program that prints exactly "a text" followed by a new line, to the standard error
+ * 
+ * Return: 1 (indicates error according to the requirement)
+ */
+int main(void)
+{
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, msg, sizeof(msg) - 1);
+
+return (1);
+}
