2cafb31a2708094a81e2c362c240fec860ff00bf
sacs28 <mi_el_28@hotmail.com>
2025-05-08 09:30:22 -0500

    Write a function that searches a string for any of a set of bytes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -28,5 +28,12 @@ void print_number(int );
 char *infinite_add(char *, char *, char *, int );
 void print_buffer(char *, int );
 char *_memset(char *, char , unsigned int );
+char *_memcpy(char *, char *, unsigned int );
+char *_strchr(char *, char );
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*)[]);
+void print_diagsums(int *, int );
 
 #endif/** MAIN_H */
