1fe51a11aea89b0ee22594743b12c5a27ef5cad6
sacs28 <mi_el_28@hotmail.com>
2025-05-08 19:07:47 -0500

    Write a function that prints the chessboard

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -1,25 +1,23 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-	char s[200];
-	char *p;
-
-	strcpy(s, "Expect the best. Prepare for the worst. ");
-	strcat(s, "Capitalize on what comes.\n");
-	strcat(s, "hello world! hello-world 0123456");
-	strcat(s, "hello world\thello world.hello world\n");
-
-	p = leet(s);
-	printf("%s", p);
-	printf("%s", s);
-
-	return (0);
+    char board[8][8] = {
+        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+    };
+    print_chessboard(board);
+    return (0);
 }
