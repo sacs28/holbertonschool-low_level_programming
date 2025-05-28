f60786daa87e5f2c8f0954eda62aa74ff36bd94f
sacs28 <mi_el_28@hotmail.com>
2025-05-27 19:51:34 -0500

    Write a function that allocates memory using malloc

---
+++ more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(unsigned int );
+char *string_nconcat(char *, char *, unsigned int );
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif/** MAIN_H */
