48857ee5fc1920c5b7e50a050a31fa6fb4e52801
sacs28 <mi_el_28@hotmail.com>
2025-06-04 20:33:58 -0500

    Write a function that prints a struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - Basic dog info
  * @name: Dog's name
@@ -15,5 +14,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif/** DOG_H */
--- structures_typedef/main_0.c
+++ structures_typedef/main_0.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include "dog.h"
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void print_dog(struct dog *d);
 
 int main(void)
 {
 	struct dog my_dog;
 
-	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
-	printf("My name is %s, and I am %.2f :) - Woof!\n", my_dog.name, my_dog.age);
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
 	return (0);
 }
