83713d6b28c1f806cdd69039753e4cd2393e5305
sacs28 <mi_el_28@hotmail.com>
2025-06-18 15:28:38 -0500

    Write a function that prints numbers, followed by a new line

---
+++ variadic_functions/main_0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_numbers("; ", 5, 30, 938, -123024, 43402, 0);
+	return (0);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,10 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
 
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
-
-#endif/** VARIADIC_FUNCTIONS */
+#endif/** VARIADIC_FUNCTIONS_H */
